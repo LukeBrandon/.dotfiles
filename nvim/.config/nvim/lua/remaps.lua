3737274c492e9ae264bf58161d726eebf89bd4e4
Luke Brandon <lukebrandon68@gmail.com>
2023-08-12 14:09:50 -0500

    couple updates

--- nvim/.config/nvim/lua/remaps.lua
+++ nvim/.config/nvim/lua/remaps.lua
@@ -4,3 +4,35 @@ vim.g.mapleader = " "
 -- Moving lines around while highlighted
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Joining lines keeps cursor location
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Centering on half page jumps
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Centering on searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Preserved the yanked buffer when pasting over something
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Delete but don't yank it
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- Yank into system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Do nothing on Q
+vim.keymap.set("n", "Q", "<nop>")
+
+-- Quick fix navigating
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
